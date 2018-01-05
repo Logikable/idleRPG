2227a60dad28e59fa27187589ec7ef0c5d598735
Logikable <spamseanluchen@gmail.com>
2018-01-05 13:06:36 -0800

    about as far as im willing to work with javascript

--- game.js
+++ game.js
@@ -11,11 +11,30 @@ const dataFile = 'data.txt'
 const adminId = 313850299838365698
 
 const xpTable = [27,46,78,133,227,318,445,623,872,1221,1466,1759,2111,2533,3040,3648,4377,5253,6304,7565,8699,10004,11505,13231,15216,17498,20123,23141,26613,30605,34277,38391,42998,48157,53936,60409,67658,75777,84870,95055,104560,115016,126518,139170,153087,168396,185235,203759,224135,246548,Infinity]
-const oreStats = {
-	iron: {xp: 2, weighing: 1, name: 'ironOre', displayName: 'iron ore'},
-	coal: {xp: 3, weighing: 1, name: 'coalOre', displayName: 'coal ore'}}
+let oreStats = {}
+let ingotStats = {}
+let drillStats = {}
 
-let totalWeighing = 0
+const items = {
+	0: {type: 'ore', oreType: 'iron', xp: 2, weighing: 1, name: 'ironOre', displayName: 'Iron Ore', level: 1},
+	1: {type: 'ore', oreType: 'coal', xp: 3, weighing: 1, name: 'coalOre', displayName: 'Coal Ore', level: 2},
+	2: {type: 'ingot', ingotType: 'iron', displayName: 'Iron Ingot', recipe: {ironOre: 2}},
+	3: {type: 'ingot', ingotType: 'steel', displayName: 'Steel Ingot', recipe: {ironOre: 2, coalOre: 1}},
+	4: {type: 'drill', drillType: 'iron', levelReq: 1, displayName: 'Iron Drill', speed: 1, mineable: ['iron', 'coal'], recipe: {ironIngot: 10}},
+	5: {type: 'drill', drillType: 'steel', levelReq: 3, displayName: 'Steel Drill', speed: 1.5, mineable: ['iron', 'coal', 'titanium'], recipe: {steelIngot: 10}},
+}
+
+var games = {}
+const gameTemplate = {
+	userId: 0,
+	ironOre: 0,
+	coalOre: 0,
+	inventory: {},
+	miningTimer: 0,
+	miningXp: 0,
+	drill: 4,
+	mute: false,
+}
 
 function xpToLevel(xp) {
 	for (let idx in xpTable) {
@@ -28,13 +47,25 @@ function xpToLevel(xp) {
 
 function xpTNL(xp) {
 	const lvl = xpToLevel(xp)
-	return xpTable[lvl] - xp
+	return xpTable[lvl - 1] - xp
 }
 
-var games = {}
 
 function load() {
 	games = JSON.parse(fs.readFileSync(dataFile))
+	// forwards compatibility
+	for (let id in games) {
+		const game = games[id]
+		for (let key in gameTemplate) {
+			if (!(key in game)) {
+				game[key] = gameTemplate[key]
+			}
+		}
+	}
+
+	for (let id in games) {
+		send(id, '**Server reloaded.**')
+	}
 }
 
 function save() {
@@ -57,27 +88,60 @@ function clear(user) {
 
 function start(user) {
 	if (!(user.id in games)) {
-		games[user.id] = {
-			userId: user.id,
-			ironOre: 0,
-			coalOre: 0,
-			miningTimer: 0,
-			miningXp: 0
-		}
+		games[user.id] = gameTemplate
+		games[user.id].userId = user.id
 	} else {
 		user.send('You already have a game! If this is an error, contact @Logikable#6019.')
 	}
 }
 
-function displayOre(user) {
+function craft(user, args) {
+	for (let key in items) {
+		
+	}
+}
+
+function addItem(game, id, quantity) {
+	if (id in game.inventory) {
+		game.inventory[id] += quantity
+	} else {
+		game.inventory[id] = quantity
+	}
+}
+
+function displayInventory(user) {
 	const game = games[user.id]
-	user.send(['**Iron Ore**: ' + game.ironOre, '**Coal Ore**: ' + game.coalOre])
+	let invMessage = []
+	for (let id in game.inventory) {
+		const item = items[id]
+		const quantity = game.inventory[id]
+		invMessage.push('**' + item.displayName + '**: ' + quantity)
+	}
+	user.send(invMessage)
 }
 
 function displayXp(user) {
 	const xp = games[user.id].miningXp
 	const level = xpToLevel(xp)
-	user.send('**Level ' + level + '** [xp: ' + xp + '/' + xpTable[level] + ']')
+	user.send('**Level ' + level + '** [xp: ' + xp + '/' + xpTable[level - 1] + ']')
+//	user.send('**Level ' + level + '** [xp: ' + xp + '/' + xpTable[level - 1] + '] - *xpTNL: ' + xpTNL(xp) + '*')
+}
+
+function toggleMute(user) {
+	games[user.id].mute = !games[user.id].mute
+	user.send(games[user.id].mute ? 'Muted.' : 'Unmuted.')
+}
+
+function displayHelp(user) {
+	const help = [
+		'**Commands for IdleRPG:**',
+		'  *start*: Starts a new game.',
+		'  *clear*: Clears your old game. **No confirmation. Be careful.**',
+		'  *stats*: Shows your stats.',
+		'  *mute*: Mutes the bot, only sending important messages.',
+		'  *ping*: Pings the server to test your connection.'
+	]
+	user.send(help)
 }
 
 client.on('message', message => {
@@ -85,38 +149,51 @@ client.on('message', message => {
 		if (message.author === client.user) {
 			return
 		}
-		const content = message.content.toLowerCase()
+		const split = message.content.toLowerCase().split(' ')
+		const command = split[0]
+		const args = split.slice(1)
 
-		if (message.author.id == adminId) {
-			if (content === 'save') {
+		if (message.author.id == adminId) {	// is mee
+			if (command === 'save') {
 				save()
 				message.author.send('Saved data to disk.')
 				console.log('Saved data to disk.')
-			} else if (content === 'load') {
+			} else if (command === 'load') {
 				load()
 				message.author.send('Loaded data from disk.')
 				console.log('Loaded data from disk.')
-			} else if (content === 'users') {
+			} else if (command === 'users') {
 				message.author.send('Current users: ' + Object.keys(games))
 			}
 		}
-		if (content === 'clear') {
-			clear(message.author)
-			message.author.send('Cleared data. Type **start** to start again.')
-			console.log('Cleared data from user ' + formatUser(message.author))
-		} else if (content === 'start') {
+
+		if (message.author.id in games) {	// has a game
+			if (command === 'clear') {
+				clear(message.author)
+				message.author.send('Cleared data. Type **start** to start again.')
+				console.log('Cleared data from user ' + formatUser(message.author))
+			} else if (command === 'ore' || command === 'inventory') {
+				displayInventory(message.author)
+			} else if (command === 'xp' || command === 'level') {
+				displayXp(message.author)
+			} else if (command === 'stats') {
+				displayXp(message.author)
+				displayInventory(message.author)
+			} else if (command === 'mute' || command === 'silence') {
+				toggleMute(message.author)
+			} else if (command === 'craft') {
+				craft(message.author, args)
+			}
+		} else if (command === 'start') {	// does not have a game
 			start(message.author)
 			message.author.send('Welcome to IdleRPG!')
 			console.log('Started a game for user ' + formatUser(message.author))
-		} else if (content === 'ore') {
-			displayOre(message.author)
-		} else if (content === 'xp' || content === 'level') {
-			displayXp(message.author)
-		} else if (content === 'stats') {
-			displayXp(message.author)
-			displayOre(message.author)
-		} else if (content === 'ping') {
+		}
+
+		if (command === 'ping') {
 			message.author.send('pong')
+		} else if (command === 'help') {
+			displayHelp(message.author)
 		}
 	}
 })
@@ -125,22 +202,38 @@ function tick() {
 	for (let userId in games) {
 		const game = games[userId]
 		let queuedXp = 0
-		if (game.miningTimer > 0 && Math.random() >= 0.8) { // successfully mined ore
+		if (game.miningTimer > 0 && Math.random() <= drillStats[game.drill].speed / 10) { // successfully mined ore
+			const level = xpToLevel(game.miningXp)
+			let totalWeighing = 0
+			for (let ore in oreStats) { // totalWeighing should only include ores mineable
+				const oreStat = oreStats[ore]
+				if (oreStat.level <= level) {
+					totalWeighing += oreStat.weighing
+				}
+			}
+
 			const rand = Math.random()
 			let sum = 0
-			for (let ore in oreStats) {
-				const oreStat = oreStats[ore]
+			for (let oreId in oreStats) {	// generate a random ore to give
+				const oreStat = oreStats[oreId]
+				if (oreStat.level > level) {	// only mine ores that can be mined
+					continue
+				}
 				sum += oreStat.weighing
-				if (rand <= sum / totalWeighing) {
-					game[oreStat.name]++
+				if (rand <= sum / totalWeighing) {	// successfully mined!
+					addItem(game, oreId, 1)
 					queuedXp += oreStat.xp
-					send(game.userId, 'You mined **' + oreStat.displayName + '** *[+' + oreStat.xp + 'xp]*.')
 					game.miningTimer = 0
+					if (!game.mute) {
+						send(game.userId, 'You mined **' + oreStat.displayName + '** *[+' + oreStat.xp + 'xp]*.')
+					}
 					break
 				}
 			}
 		} else if (game.miningTimer == 0) {
-			send(game.userId, 'You ready your pickaxe.')
+			if (!game.mute) {
+				send(game.userId, 'You ready your drill.')
+			}
 			game.miningTimer++
 		} else {
 			game.miningTimer++
@@ -154,12 +247,18 @@ function tick() {
 }
 
 client.on('ready', () => {
-	for (let ore in oreStats) {
-		const oreStat = oreStats[ore]
-		totalWeighing += oreStat.weighing
+	for (let id in items) {
+		const item = items[id]
+		if (item.type === 'ore') {
+			oreStats[id] = item
+		} else if (item.type === 'ingot') {
+			ingotStats[id] = item
+		} else if (item.type === 'drill') {
+			drillStats[id] = item
+		}
 	}
 
-	setInterval(tick, 2500)
+	setInterval(tick, 1000)
 })
 
 client.login(token)
